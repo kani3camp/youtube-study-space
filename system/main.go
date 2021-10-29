24998675a7676a7d6c1d17c6e859a9947bda7090
jun_shirasaka <sorarideblog@gmail.com>
2021-10-29 23:55:47 +0900

    Update main.go

--- system/main.go
+++ system/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"app.modules/core"
-	direct_operations "app.modules/direct-operations"
 	"context"
 	"fmt"
 	"github.com/pkg/errors"
@@ -123,12 +122,12 @@ func main() {
 	}
 	
 	// デプロイ時切り替え
-	//LocalMain(clientOption, ctx)
+	LocalMain(clientOption, ctx)
 	//Test(clientOption, ctx)
 	
 	//direct_operations.UpdateRoomLayout("../room_layouts/classroom.json", clientOption, ctx)
 	//direct_operations.ExportUsersCollectionJson(clientOption, ctx)
 	//direct_operations.ExitAllUsersAllRoom(clientOption, ctx)
-	direct_operations.ExitSpecificUser("UCN61FE7NtU0URA_u9vWWdjw", clientOption, ctx)
+	//direct_operations.ExitSpecificUser("UCN61FE7NtU0URA_u9vWWdjw", clientOption, ctx)
 }
 
