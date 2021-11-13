119325291590a010c98d7f3c0021c0a5d7fa0938
sorarideblog <sorarideblog@gmail.com>
2021-11-14 01:03:17 +0900

    fix #79

--- youtube-monitor/components/StandingRoom.tsx
+++ youtube-monitor/components/StandingRoom.tsx
@@ -7,6 +7,18 @@ class StandingRoom extends React.Component<
   any
 > {
   render () {
+    let standingUserDisplayName = '名前'
+    let standingWorkName = '作業内容'
+    let standingSeatColor = '#0000' // 初期値は透明
+    const numNoSeatUsers = this.props.no_seat_room_state?.seats.length
+    if (numNoSeatUsers !== 0) {
+      const randNum = Math.floor(Math.random() * numNoSeatUsers)
+      standingUserDisplayName = this.props.no_seat_room_state?.seats[randNum].user_display_name
+      standingWorkName = this.props.no_seat_room_state?.seats[randNum].work_name
+      standingSeatColor = this.props.no_seat_room_state?.seats[randNum].color_code
+    }
+
+
     if (this.props.no_seat_room_state) {
       const numStandingWorkers = this.props.no_seat_room_state.seats.length;
       return (
@@ -18,6 +30,14 @@ class StandingRoom extends React.Component<
             （<span className={styles.commandString}><ruby>!0<rt>{'　'}ゼロ</rt></ruby></span> で入室）
             {/*（Enter with <span className={styles.commandString}>!0</span>）*/}
           </h3>
+
+          <div className={styles.standingSeat} style={{
+            backgroundColor: standingSeatColor
+          }}>
+            <div className={styles.standingWorkName}>{standingWorkName}</div>
+            <div className={styles.standingUserDisplayName}>{standingUserDisplayName}</div>
+          </div>
+
           {/*<h2>{numStandingWorkers} People</h2>*/}
           <div id={styles.numStandingWorkers}>{numStandingWorkers}人</div>
         </div>
