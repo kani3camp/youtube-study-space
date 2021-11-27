96d85cb38ee41d0ff67d2023899d06909a05fef4
jun_shirasaka <sorarideblog@gmail.com>
2021-11-27 21:35:03 +0900

    c

--- youtube-monitor/pages/_app.tsx
+++ youtube-monitor/pages/_app.tsx
@@ -4,6 +4,7 @@ import createStore from "../store/createStore";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
     <Provider store={createStore()}>
       <Component {...pageProps} />
