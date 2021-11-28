f232cf4d4d9b5e0d209cd1d00493699cf2debcf3
jun_shirasaka <sorarideblog@gmail.com>
2021-11-28 14:36:42 +0900

    Update DefaultRoomLayout.tsx

--- youtube-monitor/components/DefaultRoomLayout.tsx
+++ youtube-monitor/components/DefaultRoomLayout.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useReducer, FC } from "react";
 import * as styles from "./DefaultRoomLayout.styles";
 import { RoomLayout } from "../types/room-layout";
-import { Seat, SeatsState } from "../types/api";
+import { Seat } from "../types/api";
 
 type Props = {
   roomLayout: RoomLayout;
@@ -170,51 +170,9 @@ const DefaultRoomLayout: FC<Props> = (props) => {
       </div>
     );
   } else {
-    return (
-      
-    )
+    return <div>Loading</div>;
   }
 
-  
-  const c = (
-    <div>
-        <div
-          css={styles.roomLayout}
-        >
-          {/* {seatList} */}
-          {
-            props.seats.map((eachSeat) => {
-              const workName = eachSeat.work_name
-              const displayName = eachSeat.user_display_name
-              const seatColor = props.seats.find(s => s.seat_id === eachSeat.seat_id)?.color_code;
-
-              return (
-                <div
-                  key={eachSeat.seat_id}
-                  css={styles.seat}
-                  style={{
-                    backgroundColor: seatColor,
-                  }}
-                >
-                  {<div css={styles.seatId} style={{ fontWeight: "bold" }}>
-                    {eachSeat.seat_id}
-                  </div>}
-                  {workName !== '' && (<div css={styles.workName}>{workName}</div>)}
-                  <div
-                    css={styles.userDisplayName}
-                  >
-                    {displayName}
-                  </div>
-
-                </div>
-
-              );
-            })
-          }
-
-        </div>
-    </div>
-  );
 }
 
 export default DefaultRoomLayout;
