5458076049418d3859e23b890defe83822121e89
sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2022-06-21 21:28:23 +0900

    Update _app.tsx

--- youtube-monitor/src/pages/_app.tsx
+++ youtube-monitor/src/pages/_app.tsx
@@ -1,16 +1,15 @@
-import { globalStyle } from '../styles/global.styles'
 import { Global } from '@emotion/react'
 import { AppProps } from 'next/app'
-import { FC } from 'react'
+import { globalStyle } from '../styles/global.styles'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  if (process.env.NEXT_PUBLIC_API_KEY === undefined) {
-    console.error('環境変数NEXT_PUBLIC_API_KEYが定義されていません')
-  }
-  return (
-    <>
-      <Global styles={globalStyle} />
-      <Component {...pageProps} />
-    </>
-  )
+    if (process.env.NEXT_PUBLIC_API_KEY === undefined) {
+        console.error('環境変数NEXT_PUBLIC_API_KEYが定義されていません')
+    }
+    return (
+        <>
+            <Global styles={globalStyle} />
+            <Component {...pageProps} />
+        </>
+    )
 }
