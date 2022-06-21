5cdfc310012e337ff7984f295cf9b6abae07c0e8
jun_shirasaka <sorarideblog@gmail.com>
2022-06-21 15:16:16 +0900

    Update _document.tsx

--- youtube-monitor/src/pages/_document.tsx
+++ youtube-monitor/src/pages/_document.tsx
@@ -1,28 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html>
-        <Head>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Inter'
-            rel='stylesheet'
-          />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap'
-            rel='stylesheet'
-          ></link>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render(): JSX.Element {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Inter'
+                        rel='stylesheet'
+                    />
+                    <link
+                        rel='preconnect'
+                        href='https://fonts.googleapis.com'
+                    />
+                    <link
+                        rel='preconnect'
+                        href='https://fonts.gstatic.com'
+                        crossOrigin='true'
+                    />
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap'
+                        rel='stylesheet'
+                    ></link>
+
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300&display=swap'
+                        rel='stylesheet'
+                    ></link>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
