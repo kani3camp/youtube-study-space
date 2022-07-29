a9d1c0c01b8c1a57feaa4a5a61d22731a1d44978
jun_shirasaka <sorarideblog@gmail.com>
2022-07-29 13:22:59 +0900

    Update BackgroundImage.tsx

--- youtube-monitor/components/BackgroundImage.tsx
+++ youtube-monitor/components/BackgroundImage.tsx
@@ -8,10 +8,14 @@ import { getCurrentSection } from "../lib/time_table";
 class BackgroundImage extends React.Component<{}, any> {
   private intervalId: NodeJS.Timeout | undefined;
   private base_url: string = 'https://source.unsplash.com/featured/1920x1080'
-  private args: string = '/?' + 'work,cafe,study,nature,chill,coffee,tea,sea,lake,outdoor,land,spring,summer,fall,winter,hotel'
-    // + ',green,purple,pink,blue,dark,azure,yellow,orange,gray,brown,red,black,pastel'
-    + ',blossom,flower,corridor,door,background,wood,resort,travel,vacation,beach,grass'
-    + ',pencil,pen,eraser,stationary,classic,jazz,lo-fi,fruit,vegetable'
+  private args: string =
+      '/?' +
+      'work,cafe,study,nature,chill,coffee,tea,sea,lake,outdoor,land,spring,summer,fall,winter,hotel' +
+      ',green,purple,pink,blue,dark,azure,yellow,orange,gray,brown,red,black,pastel' +
+      ',blossom,flower,corridor,door,background,wood,resort,travel,vacation,beach,grass' +
+      ',pencil,pen,eraser,stationary,classic,jazz,lo-fi,fruit,vegetable,sky,instrument,cup' +
+      ',star,moon,night,cloud,rain,mountain,river,calm,sun,sunny,water,building,drink,keyboard' +
+      ',morning,evening'
   private unsplash_url = this.base_url + this.args
 
   updateState () {
