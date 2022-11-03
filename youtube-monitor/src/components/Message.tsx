e268963f68c9650d51d5106ca0bf332c9dd2ccfc
jun_shirasaka <sorarideblog@gmail.com>
2022-11-03 14:39:02 +0900

    Update Message.tsx

--- youtube-monitor/src/components/Message.tsx
+++ youtube-monitor/src/components/Message.tsx
@@ -23,7 +23,7 @@ const Message: FC<Props> = (props) => {
                     })}
                 </div>
                 <div css={styles.numStudyingPeople}>
-                    {t('message.num_studying_people', { value: numWorkers })} 🎃
+                    {t('message.num_studying_people', { value: numWorkers })} 🍂
                 </div>
             </div>
         )
