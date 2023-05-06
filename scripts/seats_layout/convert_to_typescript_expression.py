67df0b035ddde3c364727b96121c138627517bb3
jun_shirasaka <sorarideblog@gmail.com>
2023-05-07 03:45:34 +0900

    Update convert_to_typescript_expression.py

--- scripts/seats_layout/convert_to_typescript_expression.py
+++ scripts/seats_layout/convert_to_typescript_expression.py
@@ -1,20 +1,21 @@
 from typing import List
 
-INPUT_FILE_NAME: str = 'seats_positions.txt'
+INPUT_FILE_NAME: str = r'C:\Users\momom\Downloads\Frame 1.txt'
 
-OFFSET_X = 535
-OFFSET_Y = -725
+OFFSET_X = 0
+OFFSET_Y = 0
+
 
 def main():
-    with open(INPUT_FILE_NAME, mode='r') as f:
+    with open(INPUT_FILE_NAME, mode='r', encoding='utf-8') as f:
         lines: List[str] = [s.rstrip('\n') for s in f.readlines()]
         # print(lines)
         for line in lines:
             items = line.split()
             assert len(items) == 3
             seat_id = items[0]
-            x = int(items[1])
-            y = int(items[2])
+            x = float(items[1])
+            y = float(items[2])
             print('{', f'id: {seat_id},x:{x+OFFSET_X},y:{y+OFFSET_Y},rotate:0,', '},')
 
 
