4f772e5a2939ca75242dfa9b14c21e915dcdfeed
Soraride <sorarideblog@gmail.com>
2024-01-28 00:54:17 +0900

    docs; update docs

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -1,133 +1,141 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
 
 const config: Config = {
-  title: 'YouTubeオンライン作業部屋 コマンド一覧',
-  tagline: 'ライブチャットに書き込もう',
-  favicon: 'img/favicon.ico',
+    title: 'YouTubeオンライン作業部屋 コマンド一覧',
+    tagline: 'ライブチャットに書き込もう',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://sorarideblog.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/youtube-study-space/',
-  trailingSlash: false,
+    // Set the production url of your site here
+    url: 'https://sorarideblog.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/youtube-study-space/',
+    trailingSlash: false,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'sorarideblog', // Usually your GitHub org/user name.
-  projectName: 'youtube-study-space', // Usually your repo name.
-  deploymentBranch: 'docusaurus',
+    // GitHub pages deployment config.
+    organizationName: 'sorarideblog', // Usually your GitHub org/user name.
+    projectName: 'youtube-study-space', // Usually your repo name.
+    deploymentBranch: 'docusaurus',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'ja',
-    locales: ['ja'],
-  },
-  
-  themes: ['@docusaurus/theme-mermaid'],
-  
-  markdown: {
-    mermaid: true,
-  },
+    i18n: {
+        defaultLocale: 'ja',
+        locales: ['ja', 'en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    themes: ['@docusaurus/theme-mermaid'],
+
+    markdown: {
+        mermaid: true,
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/sorarideblog/youtube-study-space/tree/dev/docs-site/',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'コマンド一覧',
-      logo: {
-        alt: 'Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {
-          href: 'https://github.com/sorarideblog/youtube-study-space',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
+    themeConfig: {
+        // TODO: Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'YouTubeオンライン作業部屋',
+            logo: {
+                alt: 'Site Logo',
+                src: 'img/logo.svg',
             },
-          ],
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'ドキュメント',
+                },
+                {
+                    type: 'localeDropdown',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/sorarideblog/youtube-study-space',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'ドキュメント',
+                    items: [
+                        {
+                            label: 'コマンド一覧',
+                            to: '/docs/commands',
+                        },
+                        {
+                            label: '公開資料',
+                            href: 'https://youtube-study-space.notion.site/5021213988a34747a7513f1067deb76d',
+                        },
+                    ],
+                },
+                {
+                    title: 'コミュニティ',
+                    items: [
+                        {
+                            label: 'YouTubeコミュニティ',
+                            href: 'https://www.youtube.com/channel/UCXuD2XmPTdpVy7zmwbFVZWg/community',
+                        },
+                        {
+                            label: 'YouTubeメンバーシップ',
+                            href: 'https://www.youtube.com/channel/UCXuD2XmPTdpVy7zmwbFVZWg/join',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.gg/h9SenAvawT',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://twitter.com/osr_soraride',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/sorarideblog/youtube-study-space',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} らいど開発`,
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} らいど開発`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-    colorMode: {
-      defaultMode: 'dark',
-      respectPrefersColorScheme: true,
-    },
-  } satisfies Preset.ThemeConfig,
-};
+        colorMode: {
+            defaultMode: 'dark',
+            respectPrefersColorScheme: true,
+        },
+    } satisfies Preset.ThemeConfig,
+}
 
-export default config;
+export default config
--- docs/setup.md
+++
@@ -1,9 +0,0 @@
-# Whole system setup guide
-
-## Prerequisites
-- AWS account
-- GCP Account
-
-# create AWS and GCP environment
-In youtube-study-space, the GCP (Google Cloud Platform) 
-
