4542a9d502f048d340e22bda8dada4aa624a7011
Soraride <sorarideblog@gmail.com>
2024-01-27 11:55:48 +0900

    docs; fix workflow

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://sorarideblog.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/youtube-study-space/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
