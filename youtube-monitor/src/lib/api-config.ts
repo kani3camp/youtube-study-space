f1dcbe2a12f7e2e2d7d150f9979bc8a3b8fe07c8
kani3camp <kani3camp@gmail.com>
2024-06-13 18:46:37 +0900

    update monitor: test api url

--- youtube-monitor/src/lib/api-config.ts
+++ youtube-monitor/src/lib/api-config.ts
@@ -1,5 +1,6 @@
 import { CHANNEL_GL, DEBUG } from './constants'
 
+// TODO: read url from .env
 const prodApi = {
     setDesiredMaxSeats:
         'https://1wzzml51kl.execute-api.ap-northeast-1.amazonaws.com/default/set_desired_max_seats',
@@ -7,7 +8,7 @@ const prodApi = {
 
 const testApi = {
     setDesiredMaxSeats:
-        'https://mmlcz4c490.execute-api.ap-northeast-1.amazonaws.com/default/set_desired_max_seats',
+        'https://1goygd82bk.execute-api.ap-northeast-1.amazonaws.com/default/set_desired_max_seats',
 }
 
 const glApi = {
