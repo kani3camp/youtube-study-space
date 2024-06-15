c0da1651743df2ecf54b2ae3cae7e1284019a1a1
soraride <sorarideblog@gmail.com>
2024-06-15 20:42:29 +0900

    update: <img> -> next/image

--- youtube-monitor/src/components/BackgroundImage.tsx
+++ youtube-monitor/src/components/BackgroundImage.tsx
@@ -2,6 +2,8 @@ import { FC, useState } from 'react'
 import { useInterval } from '../lib/common'
 import { getCurrentSection } from '../lib/time-table'
 import * as styles from '../styles/BackgroundImage.styles'
+import Image from 'next/image'
+import { Constants } from '../lib/constants'
 
 const BACKGROUND_UPDATE_INTERVAL_SEC = 1000
 const BACKGROUND_IMAGE_URL = '/images/background/7290504_3601460.jpg'
@@ -19,7 +21,7 @@ const BackgroundImage: FC = () => {
 
     return (
         <div>
-            <img
+            <Image
                 src={BACKGROUND_IMAGE_URL}
                 css={styles.backgroundImage}
                 alt='background image'
@@ -27,6 +29,8 @@ const BackgroundImage: FC = () => {
                     currentTarget.onerror = null // prevents looping
                     currentTarget.src = BACKGROUND_IMAGE_URL
                 }}
+                width={Constants.screenWidth}
+                height={Constants.screenHeight}
             />
             <div css={styles.blurLayer}></div>
         </div>
--- youtube-monitor/src/styles/BackgroundImage.styles.ts
+++ youtube-monitor/src/styles/BackgroundImage.styles.ts
@@ -11,7 +11,5 @@ export const backgroundImage = css`
     position: absolute;
     top: 0;
     left: 0;
-    width: ${Constants.screenWidth}px;
-    height: ${Constants.screenHeight}px;
     z-index: -1;
 `
