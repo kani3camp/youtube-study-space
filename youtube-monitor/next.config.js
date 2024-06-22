4046738dbd1e563bfd65d4135e28c10883b91660
soraride <sorarideblog@gmail.com>
2024-06-22 21:55:42 +0900

    update monitor: improve images

--- youtube-monitor/next.config.js
+++ youtube-monitor/next.config.js
@@ -3,6 +3,16 @@ const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     i18n,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'yt3.ggpht.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
