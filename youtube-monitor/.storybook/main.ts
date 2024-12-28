681afe47f420b3ac1c6382f3c62427aee339241b
kani3camp <sorarideblog@gmail.com>
2024-12-28 09:56:29 +0900

    fix storybook public path

--- youtube-monitor/.storybook/main.ts
+++ youtube-monitor/.storybook/main.ts
@@ -13,6 +13,6 @@ const config: StorybookConfig = {
         name: '@storybook/nextjs',
         options: {},
     },
-    staticDirs: ['..\\public'],
+    staticDirs: ['../public'],
 }
 export default config
