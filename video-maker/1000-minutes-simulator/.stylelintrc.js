000c60f10c98d643d5557926298bc7574ef1d160
kani3camp <sorarideblog@gmail.com>
2025-02-01 12:15:45 +0900

    biomejs 1000min-video

--- video-maker/1000-minutes-simulator/.stylelintrc.js
+++ video-maker/1000-minutes-simulator/.stylelintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    extends: [
-        'stylelint-config-standard',
-        'stylelint-config-idiomatic-order',
-        'stylelint-prettier/recommended',
-    ],
-    overrides: [
-        {
-            files: ['src/**/*.{ts,tsx}'],
-            customSyntax: '@stylelint/postcss-css-in-js',
-        },
-    ],
-    rules: {
-        'string-quotes': ['single'],
-    },
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-idiomatic-order',
+		'stylelint-prettier/recommended',
+	],
+	overrides: [
+		{
+			files: ['src/**/*.{ts,tsx}'],
+			customSyntax: '@stylelint/postcss-css-in-js',
+		},
+	],
+	rules: {
+		'string-quotes': ['single'],
+	},
 }
--- video-maker/1000-minutes-simulator/src/components/CurrentColor.tsx
+++ video-maker/1000-minutes-simulator/src/components/CurrentColor.tsx
@@ -1,54 +1,51 @@
 import { css } from '@emotion/react'
 import { useTranslation } from 'next-i18next'
-import { FC } from 'react'
+import type { FC } from 'react'
 import { MdInvertColors } from 'react-icons/md'
-import { Rank, ranks } from '../lib/ranks'
+import { type Rank, ranks } from '../lib/ranks'
 import * as styles from '../styles/CurrentColor.styles'
 import * as common from '../styles/common.styles'
 
 type Props = {
-    elapsedMinutes: number
+	elapsedMinutes: number
 }
 
 const CurrentColor: FC<Props> = (props) => {
-    const { t } = useTranslation()
+	const { t } = useTranslation()
 
-    let currentColorCode = 'inherit'
-    ranks.forEach((rank: Rank) => {
-        if (
-            rank.FromHours <= props.elapsedMinutes &&
-            props.elapsedMinutes < rank.ToHours
-        ) {
-            currentColorCode = rank.ColorCode
-        }
-    })
+	let currentColorCode = 'inherit'
+	ranks.forEach((rank: Rank) => {
+		if (
+			rank.FromHours <= props.elapsedMinutes &&
+			props.elapsedMinutes < rank.ToHours
+		) {
+			currentColorCode = rank.ColorCode
+		}
+	})
 
-    return (
-        <div css={styles.currentColor}>
-            <div css={styles.innerCell}>
-                <div css={common.heading}>
-                    <MdInvertColors
-                        size={common.IconSize}
-                        css={styles.icon}
-                    ></MdInvertColors>
-                    <span>{t('color.title')}</span>
-                </div>
-                <div
-                    css={css`
-                        ${styles.colorBox};
-                        background-color: ${currentColorCode};
-                        box-shadow: 0 0 50px ${currentColorCode};
-                    `}
-                ></div>
-                <div css={styles.annotation}>
-                    {/* 値は時間ではなく分を使うことに注意 */}
-                    {t('color.annotation_1', { value: props.elapsedMinutes })}
-                    <br />
-                    {t('color.annotation_2')}
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div css={styles.currentColor}>
+			<div css={styles.innerCell}>
+				<div css={common.heading}>
+					<MdInvertColors size={common.IconSize} css={styles.icon} />
+					<span>{t('color.title')}</span>
+				</div>
+				<div
+					css={css`
+						${styles.colorBox};
+						background-color: ${currentColorCode};
+						box-shadow: 0 0 50px ${currentColorCode};
+					`}
+				/>
+				<div css={styles.annotation}>
+					{/* 値は時間ではなく分を使うことに注意 */}
+					{t('color.annotation_1', { value: props.elapsedMinutes })}
+					<br />
+					{t('color.annotation_2')}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default CurrentColor
