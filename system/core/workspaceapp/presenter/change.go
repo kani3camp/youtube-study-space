03591efffac028070a3c9c068e65365e3ffb4ebe
kani3camp <kani3camp@gmail.com>
2025-08-11 12:33:05 +0900

    style: gofmt imports/indentation for presenter and i18n-gen

--- system/core/workspaceapp/presenter/change.go
+++ system/core/workspaceapp/presenter/change.go
@@ -3,34 +3,34 @@ package presenter
 import (
 	i18nmsg "app.modules/core/i18n/typed"
 	"app.modules/core/workspaceapp/usecase"
-    "strings"
+	"strings"
 )
 
 // BuildChangeMessage converts Change usecase events into a localized response.
 func BuildChangeMessage(res usecase.Result, displayName string) string {
-    var builder strings.Builder
-    builder.WriteString(i18nmsg.CommonSir(displayName))
-    for _, event := range res.Events {
-        switch e := event.(type) {
-        case usecase.ChangeValidationError:
-            builder.WriteString(e.Message)
-        case usecase.ChangeUpdatedWork:
-            seat := SeatIDStr(e.SeatID, e.IsMemberSeat)
-            builder.WriteString(i18nmsg.CommandChangeUpdateWork(e.WorkName, seat))
-        case usecase.ChangeUpdatedBreak:
-            seat := SeatIDStr(e.SeatID, e.IsMemberSeat)
-            builder.WriteString(i18nmsg.CommandChangeUpdateBreak(e.WorkName, seat))
-        case usecase.ChangeWorkDurationRejectedBefore:
-            builder.WriteString(i18nmsg.CommandChangeWorkDurationBefore(e.RequestedMin, e.RealtimeEntryDurationMin, e.RemainingWorkMin))
-        case usecase.ChangeWorkDurationRejectedAfter:
-            builder.WriteString(i18nmsg.CommandChangeWorkDurationAfter(e.MaxWorkTimeMin, e.RealtimeEntryDurationMin, e.RemainingWorkMin))
-        case usecase.ChangeWorkDurationUpdated:
-            builder.WriteString(i18nmsg.CommandChangeWorkDuration(e.RequestedMin, e.RealtimeEntryDurationMin, e.RemainingWorkMin))
-        case usecase.ChangeBreakDurationRejectedBefore:
-            builder.WriteString(i18nmsg.CommandChangeBreakDurationBefore(e.RequestedMin, e.RealtimeBreakDurationMin, e.RemainingBreakMin))
-        case usecase.ChangeBreakDurationUpdated:
-            builder.WriteString(i18nmsg.CommandChangeBreakDuration(e.RequestedMin, e.RealtimeBreakDurationMin, e.RemainingBreakMin))
-        }
-    }
-    return builder.String()
+	var builder strings.Builder
+	builder.WriteString(i18nmsg.CommonSir(displayName))
+	for _, event := range res.Events {
+		switch e := event.(type) {
+		case usecase.ChangeValidationError:
+			builder.WriteString(e.Message)
+		case usecase.ChangeUpdatedWork:
+			seat := SeatIDStr(e.SeatID, e.IsMemberSeat)
+			builder.WriteString(i18nmsg.CommandChangeUpdateWork(e.WorkName, seat))
+		case usecase.ChangeUpdatedBreak:
+			seat := SeatIDStr(e.SeatID, e.IsMemberSeat)
+			builder.WriteString(i18nmsg.CommandChangeUpdateBreak(e.WorkName, seat))
+		case usecase.ChangeWorkDurationRejectedBefore:
+			builder.WriteString(i18nmsg.CommandChangeWorkDurationBefore(e.RequestedMin, e.RealtimeEntryDurationMin, e.RemainingWorkMin))
+		case usecase.ChangeWorkDurationRejectedAfter:
+			builder.WriteString(i18nmsg.CommandChangeWorkDurationAfter(e.MaxWorkTimeMin, e.RealtimeEntryDurationMin, e.RemainingWorkMin))
+		case usecase.ChangeWorkDurationUpdated:
+			builder.WriteString(i18nmsg.CommandChangeWorkDuration(e.RequestedMin, e.RealtimeEntryDurationMin, e.RemainingWorkMin))
+		case usecase.ChangeBreakDurationRejectedBefore:
+			builder.WriteString(i18nmsg.CommandChangeBreakDurationBefore(e.RequestedMin, e.RealtimeBreakDurationMin, e.RemainingBreakMin))
+		case usecase.ChangeBreakDurationUpdated:
+			builder.WriteString(i18nmsg.CommandChangeBreakDuration(e.RequestedMin, e.RealtimeBreakDurationMin, e.RemainingBreakMin))
+		}
+	}
+	return builder.String()
 }
--- system/core/workspaceapp/presenter/in.go
+++ system/core/workspaceapp/presenter/in.go
@@ -3,30 +3,30 @@ package presenter
 import (
 	i18nmsg "app.modules/core/i18n/typed"
 	"app.modules/core/workspaceapp/usecase"
-    "strings"
+	"strings"
 )
 
 // BuildInMessage converts In usecase result events into a localized response message.
 func BuildInMessage(res usecase.Result, displayName string) string {
-    var builder strings.Builder
-    for _, event := range res.Events {
-        switch e := event.(type) {
-        case usecase.OrderLimitExceeded:
-            builder.WriteString(i18nmsg.CommandInTooManyOrders(e.MaxDailyOrderCount))
-        case usecase.MenuOrdered:
-            builder.WriteString(i18nmsg.CommandInOrdered(e.MenuName, e.CountAfter))
-        case usecase.SeatMoved:
-            rpEarned := ""
-            if e.RankVisible && e.AddedRP != 0 {
-                rpEarned = i18nmsg.CommandRpEarned(e.AddedRP)
-            }
-            prevSeat := SeatIDStr(e.FromSeatID, e.FromIsMemberSeat)
-            nextSeat := SeatIDStr(e.ToSeatID, e.ToIsMemberSeat)
-            builder.WriteString(i18nmsg.CommandInSeatMove(displayName, e.WorkName, prevSeat, nextSeat, e.WorkedTimeSec/60, rpEarned, e.UntilExitMin))
-        case usecase.SeatEntered:
-            seat := SeatIDStr(e.SeatID, e.IsMemberSeat)
-            builder.WriteString(i18nmsg.CommandInStart(displayName, e.WorkName, e.UntilExitMin, seat))
-        }
-    }
-    return builder.String()
+	var builder strings.Builder
+	for _, event := range res.Events {
+		switch e := event.(type) {
+		case usecase.OrderLimitExceeded:
+			builder.WriteString(i18nmsg.CommandInTooManyOrders(e.MaxDailyOrderCount))
+		case usecase.MenuOrdered:
+			builder.WriteString(i18nmsg.CommandInOrdered(e.MenuName, e.CountAfter))
+		case usecase.SeatMoved:
+			rpEarned := ""
+			if e.RankVisible && e.AddedRP != 0 {
+				rpEarned = i18nmsg.CommandRpEarned(e.AddedRP)
+			}
+			prevSeat := SeatIDStr(e.FromSeatID, e.FromIsMemberSeat)
+			nextSeat := SeatIDStr(e.ToSeatID, e.ToIsMemberSeat)
+			builder.WriteString(i18nmsg.CommandInSeatMove(displayName, e.WorkName, prevSeat, nextSeat, e.WorkedTimeSec/60, rpEarned, e.UntilExitMin))
+		case usecase.SeatEntered:
+			seat := SeatIDStr(e.SeatID, e.IsMemberSeat)
+			builder.WriteString(i18nmsg.CommandInStart(displayName, e.WorkName, e.UntilExitMin, seat))
+		}
+	}
+	return builder.String()
 }
