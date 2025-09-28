5ce48c57e9e001bc7555b35a884a50cec97d800a
kani3camp <kani3camp@gmail.com>
2025-09-28 19:19:06 +0900

    tailwind導入

--- sprint-youtube-monitor/astro.config.mjs
+++ sprint-youtube-monitor/astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwind()]
+	}
+});
