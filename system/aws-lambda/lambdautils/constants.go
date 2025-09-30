d79efd66c2645f605b8a0e70ee6a3386160967e2
kani3camp <kani3camp@gmail.com>
2025-09-30 22:20:13 +0900

    refactor

--- system/aws-lambda/lambdautils/constants.go
+++ system/aws-lambda/lambdautils/constants.go
@@ -1,8 +1,7 @@
 package lambdautils
 
 const (
-	OK                    = "ok"
-	InterruptTimeLimitSec = 13 * 60 // If a lambda function does not terminate after 13 minutes, call the next lambda function.
+	OK = "ok"
 )
 
 type UserRPParallelRequest struct {
