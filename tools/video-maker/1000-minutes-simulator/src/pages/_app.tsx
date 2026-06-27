1e3228d4ed7459f8ec6e9bbb9a73d4ed12db594c
kani3camp <kani3camp@gmail.com>
2026-06-28 02:30:11 +0900

    Align Node runtime to 24 LTS

--- tools/video-maker/1000-minutes-simulator/src/pages/_app.tsx
+++ tools/video-maker/1000-minutes-simulator/src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { Global } from '@emotion/react'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
+import type { ReactElement } from 'react'
 import { globalStyle } from '../styles/global.styles'
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
 	return (
 		<>
 			<Global styles={globalStyle} />
