9fc7542884292599495c2f1b63ac0ceabddb7173
kani3camp <kani3camp@gmail.com>
2026-08-15 02:50:38 +0900

    support durable resume preflight

--- system/core/workspaceapp/durable_message_support.go
+++ system/core/workspaceapp/durable_message_support.go
@@ -36,7 +36,7 @@ func (app *WorkspaceApp) CanProcessDurableInboxMessage(inbox repository.LiveChat
 	}
 
 	switch commandDetails.CommandType {
-	case utils.NotCommand, utils.InvalidCommand, utils.Info, utils.Seat, utils.More, utils.Rank, utils.Clear, utils.Break:
+	case utils.NotCommand, utils.InvalidCommand, utils.Info, utils.Seat, utils.More, utils.Rank, utils.Clear, utils.Break, utils.Resume:
 		return true, nil
 	default:
 		return false, nil
