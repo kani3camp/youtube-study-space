f274e0a28d799a042731e68c65eaddfc8d32929c
kani3camp <kani3camp@gmail.com>
2026-09-15 14:18:41 +0900

    refactor(youtube-monitor): make SeatAppearance V2 canonical

--- youtube-monitor/src/types/api.d.ts
+++ youtube-monitor/src/types/api.d.ts
@@ -1,14 +1,11 @@
 import type { Timestamp } from 'firebase/firestore'
 
 export type SeatAppearance = {
-	schema_version?: number
-	top_bar_color?: string
-	rank?: number
-	rank_visible?: boolean
-	color_code1: string
-	color_code2: string
+	schema_version: number
+	top_bar_color: string
+	rank: number
+	rank_visible: boolean
 	num_stars: number
-	color_gradient_enabled: boolean
 }
 
 export type Seat = {
